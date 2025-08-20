1675e3a8d5f02b20cef9c89dbed400a7e2fa55b1
grakkerfly <grakkerfly@outlook.com>
2025-08-20 13:51:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Copy contract
 function copyContract() {
-  navigator.clipboard.writeText("0x58e609879b19f90b4fdb3151bc24176b4b935b94");
+  navigator.clipboard.writeText("0xYourContractAddressHere");
   alert("Contract copied!");
 }
 
@@ -29,13 +29,54 @@ const fileName = document.getElementById('file-name');
 
 let originalImage = null;
 
-const heatmaps = [
-  ['#00008B','#0000FF','#800080','#00FF00','#FFFF00','#FF0000','#FFFFFF'],
-  ['#FF0000','#FFFF00','#00FF00','#800080','#0000FF','#00008B','#FFFFFF'],
-  ['#00FF00','#FF0000','#FFFFFF','#00008B','#0000FF','#800080','#FFFF00'],
-  ['#800080','#00008B','#FFFFFF','#FF0000','#FFFF00','#00FF00','#0000FF'],
-  ['#FFFFFF','#FF0000','#FFFF00','#00FF00','#0000FF','#00008B','#800080']
+// gradientes originais
+const heatmapsOriginal = [
+  ['#000000', '#0000FF', '#FF0000', '#FFC000', '#FFD800', '#FFFFFF'],
+  ['#0d0023', '#7800ff', '#ffeb45', '#ffffff', '#ff6000', '#ffba00'],
+  ['#120033', '#ff0000', '#7eff00', '#6c00ff', '#b478ff', '#FFFFFF'],
+  ['#060031', '#00008B', '#FF0000', '#FFF600', '#00FF00', '#0000FF'],
+  ['#000000', '#302d17', '#00118f', '#ff00de', '#fff000', '#ffffff']
 ];
+
+// funções de conversão e interpolação
+function hexToRgb(hex){
+  const bigint = parseInt(hex.replace('#',''),16);
+  return {r:(bigint>>16)&255, g:(bigint>>8)&255, b:bigint&255};
+}
+
+function hexToRgbArr(hex){
+  hex = hex.replace('#','');
+  return [parseInt(hex.substr(0,2),16), parseInt(hex.substr(2,2),16), parseInt(hex.substr(4,2),16)];
+}
+
+function rgbToHex(rgb){
+  return '#' + rgb.map(x=>Math.round(x).toString(16).padStart(2,'0')).join('');
+}
+
+function lerpColor(a,b,t){
+  return [
+    a[0]+(b[0]-a[0])*t,
+    a[1]+(b[1]-a[1])*t,
+    a[2]+(b[2]-a[2])*t
+  ];
+}
+
+// gerar gradiente suave
+function smoothGradient(colors, stepsPerSegment=20){
+  let smooth = [];
+  for(let i=0;i<colors.length-1;i++){
+    const c1 = hexToRgbArr(colors[i]);
+    const c2 = hexToRgbArr(colors[i+1]);
+    for(let s=0;s<stepsPerSegment;s++){
+      smooth.push(rgbToHex(lerpColor(c1,c2,s/stepsPerSegment)));
+    }
+  }
+  smooth.push(colors[colors.length-1]);
+  return smooth;
+}
+
+// aplicar suavização a todos os heatmaps
+const heatmaps = heatmapsOriginal.map(g=>smoothGradient(g,20));
 
 // Botão visual abre o input real
 fileButton.addEventListener('click', () => {
@@ -52,7 +93,6 @@ pfpInput.addEventListener('change', () => {
       const img = new Image();
       img.onload = function(){
         originalImage = img;
-        // aumentar tamanho do canvas
         pfpCanvas.width = img.width * 1.2;
         pfpCanvas.height = img.height * 1.2;
         ctx.drawImage(img,0,0, pfpCanvas.width, pfpCanvas.height);
@@ -76,7 +116,7 @@ function applyFilter() {
   const colors = heatmaps[filterSelect.value];
   for(let i=0;i<data.length;i+=4){
     const avg = (data[i]+data[i+1]+data[i+2])/3;
-    const colorIndex = Math.floor(avg/36) % colors.length;
+    const colorIndex = Math.floor(avg/(256/colors.length));
     const color = hexToRgb(colors[colorIndex]);
     data[i] = color.r;
     data[i+1] = color.g;
@@ -85,15 +125,9 @@ function applyFilter() {
   ctx.putImageData(imageData,0,0);
 }
 
-function hexToRgb(hex){
-  const bigint = parseInt(hex.replace('#',''),16);
-  return {r:(bigint>>16)&255,g:(bigint>>8)&255,b:bigint&255};
-}
-
 function savePFP(){
   const link = document.createElement('a');
   link.download = 'pfp.png';
   link.href = pfpCanvas.toDataURL();
   link.click();
 }
-
